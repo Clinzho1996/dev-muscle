94daefa634825cc054af70b3787f276acd5498a0
Confidence Emonena <confidinho@yahoo.com>
2023-11-14 20:16:52 +0100

    new package

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -10,11 +10,14 @@ import {
   ScrollView,
   ImageBackground,
   TouchableOpacity,
+  useWindowDimensions,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {SceneMap, TabView} from 'react-native-tab-view';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
+
 const Home = () => {
   return (
     <ScrollView style={styles.container}>
@@ -29,16 +32,143 @@ const Home = () => {
             <Text style={styles.date}>Tues 14 Nov</Text>
           </TouchableOpacity>
         </View>
-        <ImageBackground
-          source={require('../assets/bg7.png')}
-          imageStyle={{borderRadius: 20, backgroundColor: '#000', opacity: 0.7}}
-          resizeMode="cover"
-          style={styles.bg}>
-          <View>
-            <Text style={styles.day}>Day 01 - Warm Up</Text>
-            <Text style={styles.time}>07:00 - 08:00 AM</Text>
-          </View>
-        </ImageBackground>
+        <TouchableOpacity>
+          <ImageBackground
+            source={require('../assets/bg7.png')}
+            imageStyle={{
+              borderRadius: 20,
+              backgroundColor: '#000',
+              opacity: 0.7,
+            }}
+            resizeMode="cover"
+            style={styles.bg}>
+            <View>
+              <Text style={styles.day}>Day 01 - Warm Up</Text>
+              <Text style={styles.time}>07:00 - 08:00 AM</Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
+        <View style={styles.work}>
+          <Text style={styles.day}>Workout Categories</Text>
+          <TouchableOpacity>
+            <Text style={styles.date}>See All</Text>
+          </TouchableOpacity>
+        </View>
+        <ScrollView
+          contentContainerStyle={styles.category}
+          horizontal
+          showsHorizontalScrollIndicator={false}>
+          <TouchableOpacity>
+            <ImageBackground
+              source={require('../assets/bg6.png')}
+              imageStyle={{
+                borderRadius: 20,
+                backgroundColor: '#000',
+                opacity: 0.7,
+              }}
+              resizeMode="cover"
+              style={styles.bg}>
+              <View>
+                <Text style={styles.day}>Learn the Basic of Training</Text>
+                <Text style={styles.time}>06 Workouts for Beginner</Text>
+              </View>
+            </ImageBackground>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <ImageBackground
+              source={require('../assets/bg12.png')}
+              imageStyle={{
+                borderRadius: 20,
+                backgroundColor: '#000',
+                opacity: 0.7,
+              }}
+              resizeMode="cover"
+              style={styles.bg}>
+              <View>
+                <Text style={styles.day}>Full Body Goal Crusher</Text>
+                <View style={styles.proFeat}>
+                  <Text style={styles.timePro}>07 Workouts for Beginner</Text>
+                  <Text style={styles.pro}>PRO</Text>
+                </View>
+              </View>
+            </ImageBackground>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <ImageBackground
+              source={require('../assets/bg10.png')}
+              imageStyle={{
+                borderRadius: 20,
+                backgroundColor: '#000',
+                opacity: 0.7,
+              }}
+              resizeMode="cover"
+              style={styles.bg}>
+              <View>
+                <Text style={styles.day}>Wake Up Call</Text>
+                <Text style={styles.time}>04 Workouts for Beginner</Text>
+              </View>
+            </ImageBackground>
+          </TouchableOpacity>
+        </ScrollView>
+        <View style={styles.work}>
+          <Text style={styles.day}>New Workouts</Text>
+        </View>
+        <ScrollView
+          contentContainerStyle={styles.categoryTwo}
+          horizontal
+          showsHorizontalScrollIndicator={false}>
+          <TouchableOpacity>
+            <ImageBackground
+              source={require('../assets/bg13.png')}
+              imageStyle={{
+                borderRadius: 20,
+                backgroundColor: '#000',
+                opacity: 0.7,
+              }}
+              resizeMode="cover"
+              style={styles.bg}>
+              <View>
+                <Text style={styles.day}>Wake Up Call</Text>
+                <Text style={styles.time}>04 Workouts for Beginner</Text>
+              </View>
+            </ImageBackground>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <ImageBackground
+              source={require('../assets/bg12.png')}
+              imageStyle={{
+                borderRadius: 20,
+                backgroundColor: '#000',
+                opacity: 0.7,
+              }}
+              resizeMode="cover"
+              style={styles.bg}>
+              <View>
+                <Text style={styles.day}>Full Body Goal Crusher</Text>
+                <View style={styles.proFeat}>
+                  <Text style={styles.timePro}>07 Workouts for Beginner</Text>
+                  <Text style={styles.pro}>PRO</Text>
+                </View>
+              </View>
+            </ImageBackground>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <ImageBackground
+              source={require('../assets/bg10.png')}
+              imageStyle={{
+                borderRadius: 20,
+                backgroundColor: '#000',
+                opacity: 0.7,
+              }}
+              resizeMode="cover"
+              style={styles.bg}>
+              <View>
+                <Text style={styles.day}>Wake Up Call</Text>
+                <Text style={styles.time}>04 Workouts for Beginner</Text>
+              </View>
+            </ImageBackground>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
     </ScrollView>
   );
@@ -51,8 +181,9 @@ const styles = StyleSheet.create({
     height: height,
     backgroundColor: '#1C1C1E',
     paddingHorizontal: 25,
-    paddingVertical: 30,
+    paddingTop: 30,
     width: width,
+    paddingBottom: 100,
   },
   name: {
     fontSize: 30,
@@ -94,5 +225,39 @@ const styles = StyleSheet.create({
   date: {
     fontSize: 13,
     color: '#D0FD3E',
+  },
+  pro: {
+    color: '#fff',
+    padding: 5,
+    backgroundColor: '#FF2424',
+    borderRadius: 6,
+  },
+  proFeat: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  timePro: {
+    fontSize: 13,
+    color: '#D0FD3E',
+    borderLeftColor: '#FF2424',
+    borderLeftWidth: 2,
+    paddingLeft: 10,
+  },
+  category: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    gap: 10,
+    alignItems: 'center',
+  },
+  categoryTwo: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    gap: 10,
+    alignItems: 'center',
+    paddingBottom: 50,
   },
 });
